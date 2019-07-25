daaa41f681ab54f0cbddc26b2c09ddfd83797fda
Mahaveer Agarwal <mahaveer.agarwal@nagarro.com>
2019-07-25 15:25:43 +0530

    seperate file upload as a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { UploadService } from '../app/file-upload/upload.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-file-upload';
+export class AppComponent implements OnInit {
+  title: String = 'Angular File Upload';
+  uploadForm: FormGroup;
+  files = new Set<File>();
+  progress;
+  errorMessage;
+
+  config: Object = {
+    extType: 'image/*',
+    exts: ['png', 'jpg'],
+    errorMessages: {
+      success: 'Documents uploaded successfully',
+      error: 'Documents upload failed',
+      warning: 'Uploaded document has some issues',
+      extError: 'Uploaded document extension is not supported'
+    }
+  };
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private uploadService: UploadService
+  ) {}
+
+  ngOnInit() {
+    this.uploadForm = this.fb.group({
+      document: [null, null],
+      type: [null, Validators.compose([Validators.required])]
+    });
+  }
+
+  addFiles(files) {
+    this.files = files;
+  }
+
+  uploadSubmit() {
+    console.log('Mahvaeer');
+    // for (let i = 0; i < this.files; i++) {
+    //   const fileItem = this.uploader.queue[i]._file;
+    //   if (fileItem.size > 2000000) {
+    //     this.errorMessage = this.errorMessages.extError;
+    //     return;
+    //   }
+    //   const ext = fileItem.name.split('.').pop();
+    //   if (!this.exts.includes(ext)) {
+    //     this.errorMessage = this.errorMessages.extError;
+    //     return;
+    //   }
+    //   this.files.add(fileItem);
+    // }
+    this.progress = this.uploadService.upload(this.files);
+  }
 }
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,47 +1,27 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  ViewChild,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { FileUploader, FileItem } from 'ng2-file-upload';
-import { HttpClient } from '@angular/common/http';
-import { UploadService } from './upload.service';
-import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css']
 })
-export class FileUploadComponent implements OnInit {
-  uploadForm: FormGroup;
-  exts = ['png', 'jpg'];
-  errorMessage: String;
-  progress;
-  errorMessages = {
-    success: 'Documents uploaded successfully',
-    error: 'Documents upload failed',
-    warning: 'Uploaded document has some issues',
-    extError: 'Uploaded document extension is not supported'
-  };
-  public files: Set<File> = new Set();
-
+export class FileUploadComponent {
+  @Input() config: Object;
+  @Input() progress;
+  @Output() uploadFiles = new EventEmitter();
   @ViewChild('file') file;
 
+  public files: Set<File> = new Set();
   public uploader: FileUploader = new FileUploader({
     isHTML5: true
   });
-
-  title: String = 'Angular File Upload';
-  constructor(
-    private fb: FormBuilder,
-    private http: HttpClient,
-    private uploadService: UploadService
-  ) {}
-
-  ngOnInit() {
-    this.uploadForm = this.fb.group({
-      document: [null, null],
-      type: [null, Validators.compose([Validators.required])]
-    });
-  }
 
   chooseDocument() {
     this.file.nativeElement.click();
@@ -51,22 +31,11 @@ export class FileUploadComponent implements OnInit {
     console.log(event);
   }
 
-  uploadSubmit() {
+  addFiles() {
     for (let i = 0; i < this.uploader.queue.length; i++) {
       const fileItem = this.uploader.queue[i]._file;
-      if (fileItem.size > 2000000) {
-        this.errorMessage = this.errorMessages.extError;
-        return;
-      }
-      const ext = fileItem.name.split('.').pop();
-      if (!this.exts.includes(ext)) {
-        this.errorMessage = this.errorMessages.extError;
-        return;
-      }
-
       this.files.add(fileItem);
+      this.uploadFiles.emit(this.files);
     }
-
-    this.progress = this.uploadService.upload(this.files);
   }
 }
